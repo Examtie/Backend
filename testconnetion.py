bb4e986afec0e12a79c35dc80a4e832de38290fb
Regenxyz <92148448+idkwhyiusethisname@users.noreply.github.com>
2025-06-12 10:22:26 +0700

    Fix

--- testconnetion.py
+++ testconnetion.py
@@ -5,7 +5,7 @@ async def main():
     db = Prisma()
     await db.connect()
 
-    user = await db.user.create(data={'email': 'alice@example.com', 'name': 'Alice'})
+    user = await db.user.create(data={'email': 'dddoddoss@example.com', 'name': 'dododododododod'})
     print(user)
 
     await db.disconnect()
