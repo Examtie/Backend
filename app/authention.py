44dd469a1ee352c526132517b4480b24f722edc5
BreadTM <me@breadtm.xyz>
2025-06-18 21:57:32 +0700

    Corrected login exception detail

--- app/authention.py
+++ app/authention.py
@@ -50,7 +50,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     if not user or not verify_password(form_data.password, user.get("hashed_password", "")):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(
