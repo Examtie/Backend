27ce6e6060247232bb37b802f22452e3170b05b6
Regenxyz <92148448+idkwhyiusethisname@users.noreply.github.com>
2025-07-10 23:01:58 +0700

    Update default Redis URL with credentials
    The default value for REDIS_URL now includes a username and password, improving security for local development or fallback scenarios.

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,7 @@ DATABASE_NAME = os.getenv("DATABASE_NAME", "myapp")
 
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
 REDIS_DB = os.getenv("REDIS_DB", "0")
-REDIS_URL = os.getenv("REDIS_URL_KRUB", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL_KRUB", "redis://default:AS4CvtxfimJoIQznO4J7DvZGo1r0T1KcKScv48JXZQSrT6HmEcR1KMo33yrGU6NT@v4w484w4oksswk84cook0o08:6379")
 CACHE_EXPIRE_SECONDS = int(os.getenv("CACHE_EXPIRE_SECONDS", 3600))
 
 SECRET_KEY = os.getenv("SECRET_KEY", "niga56")
